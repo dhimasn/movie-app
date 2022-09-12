3a866ed8d5629a163fc7e1aea7d962b9292c6a5b
dhimasn <dhimaswift@gmail.com>
2022-09-13 03:43:26 +0700

    update massage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <HomeViews />
+  </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script> 
+import HomeViews from './views/HomeViews.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeViews
   }
 }
 </script>
---
+++ src/views/HomeViews.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home"></div>
+        <navbar />
+            <div id="main-section">
+                <sidebar />
+                <movie-list />
+            </div>>
+</template>
+<script>
+import NavBar from '../components/NavBar';
+import MovieList from '../components/MovieList';
+import SideBar from '../components/SideBar';
+export default {
+    name:"HomeViews",
+    components:{
+        NavBar,
+        MovieList,
+        SideBar
+    },
+}
+</script>
